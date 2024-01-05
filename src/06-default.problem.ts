d32bd9a3bca225b961c356de2e59fa768b877543
takakikasuga <taka.engineer.works@gmail.com>
2024-01-05 11:35:14 +0900

    Set a Default Value with Zod

--- src/06-default.problem.ts
+++ src/06-default.problem.ts
@@ -5,9 +5,12 @@ import { z } from "zod";
 
 const Form = z.object({
   repoName: z.string(),
-  keywords: z.array(z.string()).optional(),
+  keywords: z.array(z.string()).optional().default([]),
   //                           ^ 🕵️‍♂️
 });
+
+type FromInput = z.input<typeof Form>;
+type FromOutput = z.infer<typeof Form>;
 
 export const validateFormInput = (values: unknown) => {
   const parsedData = Form.parse(values);
