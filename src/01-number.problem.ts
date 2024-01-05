ad87a36d556fc68410f3f41be5f6a764058e58bc
takakikasuga <taka.engineer.works@gmail.com>
2024-01-05 11:21:57 +0900

    Runtime Type Checking with Zod

--- src/01-number.problem.ts
+++ src/01-number.problem.ts
@@ -4,8 +4,11 @@ import { expect, it } from "vitest";
 import { z } from "zod";
 //       ^ 🕵️‍♂️
 
+const numberParsed = z.number();
+
 export const toString = (num: unknown) => {
-  return String(num);
+  const parsed = numberParsed.parse(num);
+  return String(parsed);
 };
 
 // TESTS
